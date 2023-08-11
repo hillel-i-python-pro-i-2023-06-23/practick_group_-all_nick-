0df90270c56b67a30a82579b6d21effc7903b4b4
nick <nikoladarkangel@gmail.com>
2023-08-11 20:45:50 +0300

    0.0.3

--- applications/game/views.py
+++ applications/game/views.py
@@ -1,14 +1,15 @@
+from typing import List
+
 from django.shortcuts import render
 
 # from services import generate_fruits, generate_vegetables, main
 
 
 def index(request):
-    # if request.method == 'POST':
-    #     num_players = int(request.POST.get('num_players'))
-    #
-    #     players = main.add_players()
-    #
-    #     # play_game(players)
-
-    return render(request, 'index.html')
+    users_name = []
+    if request.method == "POST":
+        name = request.POST.get("participant")
+        users_name.append("name")
+        return render(request, 'index.html', f'{name}')
+    else:
+        return render(request, "index.html")
